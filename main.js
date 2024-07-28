0bec4459ffd6c2c5ae534081a85c6074ac8a027b
raffapablo <pabloraffa09@gmail.com>
2024-07-28 18:30:52 -0300

    reentrega

--- main.js
+++ main.js
@@ -1,171 +1,84 @@
-/*
-function compra (){
-let articulos = parseInt(prompt ('que desea comprar? 1:remeras / 2:pantalones /3:camperas'))
-let pantalones = 1000
-let remeras = 2000
 
-
-    switch (articulos) {
-        case 1:
-             cantidadDeArticulos = (parseInt(prompt ('que cantidad va a llevar?')))
-            alert ('Gracias por su compra. Su monto a abonar es de $'+ cantidadDeArticulos * pantalones)
-            break;
-            case 2:
-                 cantidadDeArticulos = (parseInt(prompt ('que cantidad va a llevar?')))
-                alert ('Gracias por su compra. Su monto a abonar es de $'+ cantidadDeArticulos * remeras)
-                break;
-                case 3:
-                    cantidadDeArticulos = (parseInt(prompt ('que cantidad va a llevar?')))
-                    alert ('Gracias por su compra. Su monto a abonar es de $'+ cantidadDeArticulos * camperas)
-                    break;
-    
-        default:
-            alert('el monto ingresado es erroneo')
-            break;
-    }
-}
-*/
-// function compra (){
-//     let elegirProducto = prompt ('Que desea comprar?');
-//     switch (elegirProducto){
-//         case 'hola':
-//             alert('daleeee de una ahi va')
-//           break
-                
-//                 default: 
-//                 alert ('ah ')
-//     } 
-// }
-
-
-
-            
-        //  cantidad = parseInt(prompt ('Que cantidad de bolsas de papel madera desea llevar?' ))
-        //  total = alert('Gracias por su compra, su total es de $' + papelMadera * cantidad)
-        // break
-
-        // case '3':
-        //      cantidad = parseInt(prompt ('Que cantidad de bolsas de residuos desea llevar?' ))
-        //      total = alert('Gracias por su compra, su total es de $' + residuos * cantidad)
-        //     break
-
-        //     case '4':
-        //  cantidad = parseInt(prompt ('Que cantidad de bolsas de friselina desea llevar?' ))
-        //  total = alert('Gracias por su compra, su total es de $' + friselina * cantidad)
-        // break
-
-        // case '5':
-        //  cantidad = parseInt(prompt ('Que cantidad de bolsas de arranque desea llevar?' ))
-        //  total = alert('Gracias por su compra, su total es de $' + arranque * cantidad)
-
-        // break
-
-        // default:
-        //     alert('el valor ingresado es incorrecto. \n recuerde que debe ingresar un valor numerico del 1 al 5')
+                                     /* BIENVENIDA Y RECOPILACION DE DATOS */
+ 
+ function newUser () {
+    let saludar = confirm('¡Bienvenido! si es la primera vez que compra en Palo y a la Bolsa; ingrese su: \n\n -Nombre \n\n -Apellido \n\n -DNI')
+    let nombre = ''
+    let apellido = ''
+    let dni = 0
         
+        if (saludar == true){
+            nombre = prompt('Ingrese su nombre:')
+            apellido = prompt('ingrese su apellido')
+            dni = parseInt(prompt ('ingrese su dni sin puntos, espacios ni guiones:'))
+            const confirmar = confirm('Confirme los siguientes datos: \n\n Nombre: ' + nombre + '\n Apellido: ' + apellido + '\n Dni: ' + dni)
+                if(confirmar == true){
+                    const gracias = alert ('Gracias ' + nombre + ', para realizar tu primer compra presiona el boton "Comprar productos"')
+                }
+                else {
+                    while(confirmar == false){
+                    const deNuevo = confirm ('Ok, vamos de nuevo')
+                           if(deNuevo == true)  {
+                                nombre = prompt('Ingrese su nombre:')
+                                apellido = prompt('ingrese su apellido')
+                                dni = parseInt(prompt ('ingrese su dni sin puntos, espacios ni guiones:'))
+                                confirmar = confirm('Confirme los siguientes datos: \n\n Nombre: ' + nombre + '\n Apellido: ' + apellido + '\n Dni: ' + dni)
+                                gracias = alert ('Gracias ' + nombre + ', para realizar tu primer compra presiona el boton "Comprar productos"')
+                            } 
+                            else {
+                             alert('Vuelva cuando guste.')
+                              }
+                            break
+                }
+                }
+        }
+        else{
+            const vuelvaCunadoGuste = alert ('Ok, cuando quiera puede registrarse presionando el boton "Crear cuenta"')
+        }
+    }
+    /*Profe, en esta parte me cuesta hacer un bucle infinito para que cada vez que el usuario ponga cancelar se reactive la funcion de ingresar los datos. mire las clases y busque informacion pero no me termina de quedar bien y sinceramente, no quiero que chat gpt me genere el codigo. */
+                    /*le agradeceria si me podria dejar un ejemplo en la devolucion o poder verlo en clase. Muchas gracias */
+    /*Tambien queria decirle que no se si es una buena practica meter un if dentro de otro if, pero es la unica forma en la que pude hacer que dentro de todo tenga una buena funcionalidad con el usuario */
+    /*Desde ya le pido disculpas si es una burrada lo que hice.*/
 
-
-                                     /* BIENVENIDA */
-let saludar = confirm('¡Bienvenido! si es la primera vez que compra en Palo y a la Bolsa; ingrese su: \n\n -Nombre \n\n -Apellido \n\n -DNI')
-                                     /*ARRAY DE PRODUCTOS Y VALORES DE LOS MISMOS*/
-const productos = ["\n\n 1) Polietileno ($300 c/u)" , " \n\n 2) Papel madera ($350 c/u)" , '\n\n 3) Residuos ($400 c/u)' , '\n\n 4) Friselina ($450 c/u)' , '\n\n 5) Arranque ($500 c/u)' ,]
-const polietileno = 300
-const papelMadera = 350
-const residuos = 400
-const friselina = 450
-const arranque = 500
-                                    /*RECOPILACION DE DATOS */
-
-
-if (saludar == true) {
-
-    let nombre = prompt('Ingrese por favor su nombre:')
-    let apellido = prompt('Ingrese por favor su apellido:')
-    let dni = parseInt (prompt('Ingrese por favor su dni:'))
-    const confirmar = alert('Hola '+ nombre + ', gracias por registrarte, por favor confirma los siguientes datos: \n -Nombre: '+ nombre + '\n -Apellido: ' + apellido + '\n -DNI: ' + dni )
-    let gracias = alert ('Gracias ' + nombre + ', para realizar tu primer compra presiona el boton "Comprar productos"') 
-                          /* este console.log() tiene la unica funcion de cumplir con las consignas de la pre-entrega*/ 
-    console.log(nombre  + '\n' + apellido + '\n' + dni)   
-}   
-else {
-
-     alert ('No hay problema, vuelva cuando guste. \n\n -Si fue un error y desea comprar en este momento, solo refresque la tienda web (F5) \n\n -Si presiona "aceptar" podra seguir navegando por la tienda, gracias.')
-} 
-
-
-                                 /* FUNCION CARRITO DE COMPRAS Y CALCULADOR DE PRECIO FINAL*/
-function compra(){
+    const productos = ["\n\n 1) Polietileno ($300 c/u)" , " \n\n 2) Papel madera ($350 c/u)" , '\n\n 3) Residuos ($400 c/u)' , '\n\n 4) Friselina ($450 c/u)' , '\n\n 5) Arranque ($500 c/u)' ,]
     const polietileno = 300
     const papelMadera = 350
     const residuos = 400
     const friselina = 450
     const arranque = 500
-    cantidad = 0
-    let producto = parseInt(prompt ('¿Que desea comprar? Ingrese un valor numerico del 1 al 5' + productos))
-    switch(producto){
-        case 1:
-            cantidad = parseInt(prompt('¿que cantidad de bolsas de polietileno desea llevar?'))
-            alert('¡GRACIAS POR SU COMPRA! \n\n su total es de $' + polietileno * cantidad + '\n Para poder abonar su compra presione el boton "pagar"')
-            total = polietileno *cantidad
-            break
-        case 2:
-            cantidad = parseInt(prompt('¿que cantidad de bolsas de papel madera desea llevar?'))
-            alert('¡GRACIAS POR SU COMPRA! \n\n su total es de $' + papelMadera * cantidad + '\n Para poder abonar su compra presione el boton "pagar"' )
-            total = papelMadera * cantidad
-            break
-        case 3:
-            cantidad = parseInt(prompt('¿que cantidad de bolsas de residuos desea llevar?'))
-            alert('¡GRACIAS POR SU COMPRA! \n\n su total es de $' + residuos * cantidad + '\n Para poder abonar su compra presione el boton "pagar"' )
-            total = residuos *cantidad
-            break
-        case 4:
-            cantidad = parseInt(prompt('¿que cantidad de bolsas de friselina desea llevar?'))
-            alert('¡GRACIAS POR SU COMPRA! \n\n su total es de $' + friselina * cantidad + '\n Para poder abonar su compra presione el boton "pagar"' )
-            total = friselina *cantidad
-            break
-        case 5:
-            cantidad = parseInt(prompt('¿que cantidad de bolsas de arranque desea llevar?'))
-            alert('¡GRACIAS POR SU COMPRA! \n\n su total es de $' + arranque * cantidad + '\n Para poder abonar su compra presione el boton "pagar"' )
-            total = arranque * cantidad
-            break
+    let  cantidad = 0
+    let producto = 0
+    let total = 0
 
-        default:
-            alert('No ingreso un dato correcto.\n\n\n Recuerde ingresar un numero del 1 al 5 \n presione nuevamente el boton de "comprar productos" ')
+    pago = (producto, cantidad) =>{
+        cantidad = parseInt(prompt('¿que cantidad de bolsas de polietileno desea llevar?'))
+        total = producto * cantidad
+        alert('¡GRACIAS POR SU COMPRA! \n\n su total es de $' + total)
     }
-    
-}
 
-let mercadoPago = 1.06
-let debito = 1.10
-let efectivo = 1
-let transferencia = 1.2
-const pagar = () => {
-    let monto = 0
-   let formaDePago = prompt('Seleccione su metodo de pago: \n\n -Efectivo (mismo precio) \n -Debito (10% de recargo) \n -Mercado pago (6% de recargo) \n -Transferencia (20% de DESCUENTO)')
-                                /*tolowercase para minimizar errores*/
-   formaDePago.toLocaleLowerCase
-   switch(formaDePago){
-    case 'efectivo':
-        monto = parseInt(prompt ('Ingrese el monto de su compra'))
-        alert ('Su pago a abonar es de $' + monto * efectivo)
-        break
-    case 'debito':
-         monto = parseInt(prompt ('Ingrese el monto de su compra'))
-        alert ('Su pago a abonar es de $' + monto * debito)
-        break
-    case 'mercado pago':
-        monto = parseInt(prompt ('Ingrese el monto de su compra'))
-        alert ('Su pago a abonar es de $' + monto * mercadoPago)
-        break
-    case 'transferencia':
-         monto = parseInt(prompt ('Ingrese el monto de su compra'))
-        alert ('Su pago a abonar es de $' + monto / 100 * 80)
-        break
-    default: alert ('Error \n Ingrese nuevamente el monto y la forma de pago \n\n\n En este punto NO debe escribir un caracter numerico del 1 al 5 si no que debe escribir la opcion deseada \n Ejemplo: "efectivo" ')
-    
-   }
-}
+    function compra(){
+        producto = parseInt(prompt ('¿Que desea comprar? Ingrese un valor numerico del 1 al 5' + productos))
 
-
-
-
+        switch(producto){
+            case 1:
+                 pago(300, cantidad)
+                break
+            case 2:
+                pago  (350, cantidad)
+                break
+            case 3:
+                pago  (400, cantidad)
+                break
+            case 4:
+                pago  (450, cantidad)
+                break
+            case 5:
+                pago  (500, cantidad)
+                break
+            default:
+             const datoIncorrecto =  alert('No ingreso un dato correcto.\n\n\n Recuerde ingresar un numero del 1 al 5 \n presione nuevamente el boton de "comprar productos" ')
+                break
+            }
+        
+    }
